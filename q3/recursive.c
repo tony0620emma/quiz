3589ed23cf0610949f69d906f8f85afb04176f01
tonyyanxuan <tony0620emma@yahoo.com.tw>
2015-10-18 19:11:06 +0800

    Implement q3 recursive version, need test cases

--- q3/recursive.c
+++ q3/recursive.c
@@ -1,4 +1,20 @@
-/* FIXME: Implement! */
+#include "tree.h"
+
+void flatten(struct TreeNode *root)
+{
+	if (!root)
+	  	return;
+	if (root->left) {
+		struct TreeNode *temp = root->left;
+		while (temp->right)				
+			temp = temp->right;			
+		temp->right = root->right;			
+		root->right = root->left;			
+		root->left = NULL;			
+	}
+	flatten(root->right);
+}
+
 
 int main()
 {
