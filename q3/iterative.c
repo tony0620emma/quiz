77e429e8acf07c9cc9b97cfaf9c79986120f94d6
tonyyanxuan <tony0620emma@yahoo.com.tw>
2015-10-18 19:00:04 +0800

    Implement q3 iterative version, need further test cases

--- q3/iterative.c
+++ q3/iterative.c
@@ -1,4 +1,21 @@
-/* FIXME: Implement! */
+#include "tree.h"
+
+void flatten(struct TreeNode *root)
+{
+	struct TreeNode *temp;
+	while (root) {
+		if (root->left) {
+			temp = root->left;
+			while (temp->right) {
+				temp = temp->right;
+			}
+			temp->right = root->right;
+			root->right = root->left;
+			root->left = NULL;
+		}
+		root = root->right;
+	}
+}
 
 int main()
 {
