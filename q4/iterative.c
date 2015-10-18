98ceb79b104a1c056c2a1243be6102828d32a537
tonyyanxuan <tony0620emma@yahoo.com.tw>
2015-10-18 19:19:49 +0800

    Implement q4 iterative version, need test cases

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,4 +1,17 @@
-/* FIXME: Implement! */
+int maxSubArray(int *nums, int numsSize) 
+{
+	int temp = 0;
+	int max = nums[0];
+	for (inti = 0; i < numsSize; i++) {
+		if (temp > 0)
+			temp += nums[i];
+		else
+			temp = nums[i];
+		if (temp > max)
+			max = temp;
+	}
+	return max;
+}
 
 int main()
 {
